2ea0b480120844a7a52687853f28649e7075ac63
Roman Isaev <mdnight@riseup.net>
2023-03-28 21:43:58 +0100

    add more keywords

--- token/token.go
+++ token/token.go
@@ -33,6 +33,11 @@ const (
 	// Keywords
 	FUNCTION TokenType = "FUNCTION"
 	LET      TokenType = "LET"
+	IF       TokenType = "IF"
+	ELSE     TokenType = "ELSE"
+	TRUE     TokenType = "TRUE"
+	FALSE    TokenType = "FALSE"
+	RETURN   TokenType = "RETURN"
 )
 
 type Token struct {
@@ -41,8 +46,13 @@ type Token struct {
 }
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"if":     IF,
+	"else":   ELSE,
+	"true":   TRUE,
+	"false":  FALSE,
+	"return": RETURN,
 }
 
 func LookupIndent(literal string) TokenType {
